23c90dd638b501edde5619929509faf4b9698441
neeraj-s-bhadouria <neerajsb28@gmail.com>
2023-08-28 23:28:25 +0530

    Linear Search Implemented

---
+++ src/com/neeraj/dsa/searching/LinearSearch.java
@@ -0,0 +1,13 @@
+package com.neeraj.dsa.searching;
+
+public class LinearSearch {
+
+	public int linearSearch(int nums[], int target) {
+		for(int i=0; i<nums.length; i++) {
+			if(nums[i] == target) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}
